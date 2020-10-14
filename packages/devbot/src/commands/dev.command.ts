f2c6fe6ccfb325cf6725486838f472b89c09cee0
Edixon Piña <edixonalbertto@gmail.com>
2020-10-13 23:52:57 -0400

    installing new module to create alias in node
    - refactor in response in dev.command

--- packages/devbot/src/commands/dev.command.ts
+++ packages/devbot/src/commands/dev.command.ts
@@ -1,5 +1,5 @@
 import { langList, colorsList } from '../enumerations';
-import { Format } from '../helpers';
+import { Format } from '~HELP/Format';
 import { ScrapeMozilla } from '../modules/ScrapeMozilla';
 
 const HEADER: string = 'DEV';
@@ -10,48 +10,45 @@ export const dev = async ({ content, response }: TCommand): Promise<void> => {
   let data: TDataDev;
   console.log(data);
 
-  if (langList[lang as TLang]) {
-    switch (lang as TLang) {
-      case 'js':
-        title = 'JavaScript';
-        data = await ScrapeMozilla.getMethod(method);
-        break;
+  switch (lang as TLang) {
+    case 'js':
+      title = 'JavaScript';
+      data = await ScrapeMozilla.getMethod(method);
+      break;
 
-      default:
-        break;
-    }
-
-    if (data) {
-      response.embeded({
-        header: HEADER,
-        title: `${title} \`${method}()\``,
-        detail: [
-          {
-            title: 'Definición',
-            content: `${data.definition}`,
-            fieldType: 'row'
-          },
-          {
-            title: 'Ejemplo',
-            content: Format.code('js', data.example),
-            fieldType: 'row'
-          }
-        ],
-        color: colorsList.javascript
-      });
-    } else {
+    default:
       response.embeded({
         header: HEADER,
         title: 'Error',
-        detail: `El metodo \`${method}()\` no existe`,
+        detail: `\`${lang}\` no es un lenguaje valido`,
         color: colorsList.error
       });
-    }
+      break;
+  }
+
+  if (data) {
+    response.embeded({
+      header: HEADER,
+      title: `${title} \`${method}()\``,
+      detail: [
+        {
+          title: 'Definición',
+          content: `${data.definition}`,
+          fieldType: 'row'
+        },
+        {
+          title: 'Ejemplo',
+          content: Format.code('js', data.example),
+          fieldType: 'row'
+        }
+      ],
+      color: colorsList.javascript
+    });
   } else {
     response.embeded({
       header: HEADER,
       title: 'Error',
-      detail: `\`${lang}\` no es un lenguaje valido`,
+      detail: `El metodo \`${method}()\` no existe`,
       color: colorsList.error
     });
   }
--- packages/devbot/src/enumerations.ts
+++ packages/devbot/src/enumerations.ts
@@ -1,9 +1,9 @@
-export enum colorsList {
+export const enum colorsList {
   ok = '#92D050',
   error = '#ED1C24',
   javascript = '#F7DF1E'
 }
 
-export enum langList {
+export const enum langList {
   js = 'JavaScript'
 }
