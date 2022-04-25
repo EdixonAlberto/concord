7803cd7069ad807e15ae09522a00c6965fac10ba
Edixon <edixonalbertto@gmail.com>
2022-04-24 21:56:22 -0400

    version 1.7.1
    - remove console.log and use logger

--- packages/library/src/utils/config.util.ts
+++ packages/library/src/utils/config.util.ts
@@ -1,7 +1,7 @@
+import { logger } from '~UTILS'
+
 export async function configLoad(): Promise<void> {
   if (typeof process.env.TOKEN === 'undefined') {
-    console.log('LOAD')
-
     const { access } = await import('fs/promises')
     const { resolve, basename } = await import('path')
     const { config } = await import('dotenv')
@@ -18,7 +18,7 @@ export async function configLoad(): Promise<void> {
       const result = config({ path })
 
       if (result.error) throw new Error(`ERROR-ENV -> ${result.error.message}`)
-      else console.log(`ENV -> Environment "${basename(path)}" loaded successfully`)
+      else logger('ENV', `Environment "${basename(path)}" loaded successfully`)
     }
   }
 }
